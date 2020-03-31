4b8bb6c04c6fd3e24f95366e4bca1f08c8bc6539
kimeunyoung01 <rola70@nate.com>
2020-03-31 15:10:26 +0900

    insert, login 처리

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -1,7 +1,15 @@
 package com.example.demo.dao;
 
 public class MemberDao {
-
+	
+	public void login() {
+		System.out.println("로그인하였습니다.");
+	}
+	
+	public void insertMember() {
+		System.out.println("회원을 등록하였습니다.");
+	}
+	
 	public void listMember() {
 		System.out.println("회원의 정보를 수정했습니다.");
 	}
