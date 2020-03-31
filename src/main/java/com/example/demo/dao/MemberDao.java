ae2c017bb97b003a9f1f96975787c908ec90a90b
kimeunyoung01 <rola70@nate.com>
2020-03-31 14:56:45 +0900

    deleteMember 추가함.

--- src/main/java/com/example/demo/dao/MemberDao.java
+++ src/main/java/com/example/demo/dao/MemberDao.java
@@ -3,10 +3,14 @@ package com.example.demo.dao;
 public class MemberDao {
 
 	public void listMember() {
-		System.out.println("member");
+		System.out.println("회원의 정보를 수정했습니다.");
 	}
 	
 	public void updateMember() {
+		System.out.println("회원의 정보를 수정했습니다..");
+	}
+	
+	public void deleteMember() {
 		System.out.println("회원의 정보를 삭제하였습니다.");
 	}
 }
